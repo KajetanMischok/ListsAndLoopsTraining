5a85397dc9c38c7edeef70b952f8de6524c23c2e
Kajetan Mischoke <kajetan.mischok@mischok.de>
2021-10-01 08:02:11 +0200

    Addded Fun with primes

---
+++ ListsAndLoops/src/main/java/FunWithPrimes.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunWithPrimes {
+    /**
+     * Returns true if the given int is a prime number.
+     * @param number number to be tested
+     * @return true if prime, otherwise false
+     */
+    public static boolean isPrime(int number){
+        return false;
+    }
+
+    /**
+     * Returns true if both numbers are prime numbers
+     * @param numberOne
+     * @param numberTwo
+     * @return true if both primes
+     */
+    public static boolean bothPrime(int numberOne, int numberTwo){
+        return false;
+    }
+
+    /**
+     * Returns true if all numbers are prime numbers
+     * @param numberOne
+     * @param numberTwo
+     * @param numberThree
+     *
+     * @return true if all primes
+     */
+    public static boolean allPrime(int numberOne, int numberTwo, int numberThree){
+        return true;
+    }
+
+    /**
+     * Returns true if one number is a prime number
+     * @param numberOne
+     * @param numberTwo
+     * @param numberThree
+     *
+     * @return true if one number is a prime number
+     */
+    public static boolean isOnePrime(int numberOne, int numberTwo, int numberThree){
+        return false;
+    }
+
+    /**
+     * Returns a list with all prime number within the range from 10 to 10000
+     * @return prime number list
+     */
+    public static List<Integer> getPrimesFrom10to10000(){
+        return new ArrayList<>();
+    }
+
+    /**
+     * Retruns a list with every second prime in the range of 100 to 10
+     * @return list with primes
+     */
+    public static List<Integer> getEverySecondPrimeFrom100to10(){
+        return new ArrayList<>();
+    }
+}
